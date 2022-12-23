ad01a5cef7a06827805037711e61b1285f4d001e
Janis Salins <janis.salins@gmail.com>
2022-12-23 09:13:13 +0200

    content update

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
   import icon_red from "./assets/creators/sensus-icon-red.svg";
   import icon_dark from "./assets/creators/sensus-icon-dark.svg";
   import header_bg from "./assets/creators/header-bg.png"
+  import about_bg from "./assets/creators/about-bg.png"
 
   import ListItemLarge from "./components/ListItemLarge.svelte";
 
@@ -27,8 +28,7 @@
 
 <main>
   <header
-    class="container bg-snaccent self-center flex flex-col bg-fixed bg-cover bg-left bg-no-repeat" 
-    style="height: 80vh; background-image: url({header_bg})"
+    class="container bg-snaccent self-center flex flex-col bg-fixed bg-cover bg-left bg-no-repeat" style="height: 80vh; background-image: url({header_bg})"
   >
     <nav>
       <div class="mx-auto flex items-center justify-between md:h-10">
@@ -58,33 +58,34 @@
     </div>
   </header>
 
-  <section id="praescriptus" class="container bg-sn-light">
-    
-    <div class="flex pt-7 pb-7 md:pb-10">
-      <img src={icon_red} class="mr-3 w-10" alt="">
-    <h3 class="text-snaccent text-4xl md:text-5xl font-bold tracking-tighter whitespace-nowrap">
-      Praescriptus <sup class="text-sm">TM</sup>
-    </h3>
-    </div>
-    <div class="md:flex flex-1 justify-center">
-      <div class="md:w-1/2">
-        <p class="text-snaccent md:max-w-sm md:pl-14">
-          We examine each issue distinctively and create a unique potion by
-          following our six senses.
-        </p>
+  <section id="praescriptus" class="container bg-sn-light" >
+    <div class="md:pb-64 bg-cover md:bg-[length:70%_auto] bg-right-top md:bg-right md:bg-center bg-no-repeat" style="background-image: url({about_bg})">
+      <div class="flex pt-7 pb-7 md:pb-10">
+        <img src={icon_red} class="mr-3 w-10" alt="">
+        <h3 class="text-snaccent text-4xl md:text-5xl font-bold tracking-tighter whitespace-nowrap">
+        Praescriptus <sup class="text-sm">TM</sup>
+        </h3>
       </div>
-      <div class="md:w-1/2">
-        <h1 class=" text-snaccent text-7xl font-bold pt-20 pb-7 md:pb-10">
-          <i class="font-caption">How</i> it works
-        </h1>
-        <p class="text-snaccent md:max-w-md">
-          Our six-step framework is something that experts have been using in
-          medicine and other scientific fields where precision is essential. We
-          believe that PR shouldn't be treated any differently.
-        </p>
+      <div class="md:flex flex-1 justify-center ">
+        <div class="md:w-1/2">
+          <p class="text-snaccent md:max-w-sm md:pl-14">
+            We examine each issue distinctively and create a unique potion by
+            following our six senses.
+          </p>
+        </div>
+        <div class="md:w-1/2">
+          <h1 class=" text-snaccent text-7xl font-bold pt-20 pb-7 md:pb-10">
+            <i class="font-caption">How</i> it works
+          </h1>
+          <p class="text-snaccent md:max-w-md">
+            Our six-step framework is something that experts have been using in
+            medicine and other scientific fields where precision is essential. We
+            believe that PR shouldn't be treated any differently.
+          </p>
+        </div>
       </div>
     </div>
-    <div class="sensus-list my-20 mx-auto py-20 max-w-4xl flex flex-col items-center justify-center gap-10 shadow-hard">
+    <div class="sensus-list my-20 mx-auto py-20 max-w-5xl flex flex-col items-center justify-center gap-10 shadow-hard">
       <div class="flex justify-items-center"><img src="{ icon_dark }" alt="" class="mr-2"><p class="text-4xl font-bold tracking-tight">Praescriptus<sup>TM</sup></p></div>
 
       {#each list_items as { title, description }, id}
@@ -96,12 +97,12 @@
     </div>
   </section>
 
-  <section id="makers" class="container bg-sn-dark">
-    <div class="w-1/2">
-      <h1 class="text-sn-light text-8xl font-bold">
-        There are no two identical problems
+  <section id="makers" class="container bg-sn-dark md:p-20">
+    <div class="md:w-2/3">
+      <h1 class="text-sn-light text-6xl md:text-8xl font-bold">
+        There are no two <i class=" font-caption">identical</i>  problems
       </h1>
-      <p class="text-sn-light">
+      <p class="text-sn-light max-w-sm py-20">
         We create tailor-made solutions by relying on top-tier marketing
         expertise and common sense.
       </p>
@@ -109,22 +110,22 @@
     <h3 class="text-center text-sn-light text-3xl font-bold my-8">
       Meet our board of sense-makers
     </h3>
-    <div class="md:grid md:grid-cols-2 lg:grid-cols-3 justify-items-center">
-      <div class="max-w-sm">
+    <div class="md:grid md:grid-cols-2 lg:grid-cols-3 justify-items-center md:gap-10">
+      <div class="max-w-sm mb-10">
         <img class="mb-5" src={c1} alt="" />
         <p class=" text-sn-light">Ieva Gorkša</p>
         <p class=" text-sn-light font-bold">Business Development Director</p>
         <p class=" text-sn-light">ieva.gorksa@sensus.lv</p>
         <p class=" text-sn-light">+371 29 117 181</p>
       </div>
-      <div class="max-w-sm">
+      <div class="max-w-sm mb-10">
         <img class="mb-5" src={c2} alt="" />
         <p class=" text-sn-light">Iveta Puntuze</p>
         <p class=" text-sn-light font-bold">Financial Director</p>
         <p class=" text-sn-light">iveta.puntuze@sensus.lv</p>
         <p class=" text-sn-light">+371 29 646 255</p>
       </div>
-      <div class="max-w-sm">
+      <div class="max-w-sm mb-10">
         <img class="mb-5" src={c3} alt="" />
         <p class=" text-sn-light">Kristiāna Pinne</p>
         <p class=" text-sn-light font-bold">Account Manager</p>
@@ -136,25 +137,24 @@
 
   <footer
     class="container bg-snaccent self-center flex flex-col"
-    style="height: 70vh;"
   >
-    <div class="flex flex-col w-1/2 flex-1 justify-center">
-      <h1 class="text-white text-8xl font-bold">
+    <div class="flex flex-col w-2/3 flex-1 justify-center">
+      <h1 class="text-white text-7xl md:text-8xl font-bold md:ml-20 py-10">
         Let's start making <i class=" font-caption">sense!</i>
       </h1>
     </div>
-    <div class="flex w-2/4">
+    <div class="md:flex md:w-7/12">
       <div class=" w-full">
-        <p class="text-sn-light">
+        <p class="text-sn-light max-w-xs md:ml-20">
           Contact us for a consultation and get your prescription.
         </p>
       </div>
-      <div>
+      <div class="mt-10 md:mt-0">
         <p class="text-sn-light">info@sensus.lv</p>
         <p class="text-sn-light">+371 29 117 181</p>
       </div>
     </div>
-    <h4 class="text-sn-light font-bold tracking-[0.45rem]">MAKING SENSE</h4>
+    <h4 class="text-sn-light font-bold tracking-[0.45rem] mt-20 mb-10">MAKING SENSE</h4>
   </footer>
 </main>
 
--- src/components/ListItemLarge.svelte
+++ src/components/ListItemLarge.svelte
@@ -4,17 +4,17 @@
     export let title;
     export let description;
 </script>
-<div class="flex items-center w-full justify-between max-w-3xl">
+<div class="flex items-center w-full justify-between ">
   <div class="w-1/3">
-    <p class="text-2xl font-bold text-right">{@html title}</p></div>
+    <p class="text-3xl font-bold text-right">{@html title}</p></div>
   <div class="w-1/3 relative flex justify-center">
-    <img class="rounded-full w-16" src={listbg} alt="" />
+    <img class="rounded-full w-20" src={listbg} alt="" />
     <span
       class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl font-bold">{id}
     </span>
   </div>
   <div class="w-1/3">
-    <p class=" max-w-xs">{@html description}</p>
+    <p class="text-md max-w-xs">{@html description}</p>
   </div>
 </div>
 <style>
